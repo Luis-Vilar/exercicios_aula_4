483c681f3980ae18cf6838c0fb0c5f74478b78bd
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-05-18 22:40:58 -0300

    Feat : Add basic tests

--- test/exercicios_aula_4_test.dart
+++ test/exercicios_aula_4_test.dart
@@ -1,7 +1,26 @@
 import 'package:test/test.dart';
+import 'package:exercicios_aula_4/patients.model.dart';
 
 void main() {
-  test('calculate', () {
-    expect(true, true);
+  final Map<String, dynamic> map = {
+    "paciente": "Clayton",
+    "medico": "Dra. Maria",
+    "exames": ["Hemograma", "Glicemia"],
+    "data": "1983-06-17",
+  };
+
+  PatientModel patientModel = PatientModel.fromMap(map);
+
+  test('patientModel.name Should by equals to map["paciente"]', () {
+    expect(patientModel.name, map['paciente']);
+  });
+  test('patientModel.doctor Should by map["medico"] ', () {
+    expect(patientModel.doctor, map['medico']);
+  });
+  test('patientModel.tests Should by map["exames"] ', () {
+    expect(patientModel.tests, map['exames']);
+  });
+  test('patientModel.dateTime Should by DateTime.parse(map["data"] ', () {
+    expect(patientModel.dateTime, DateTime.parse(map["data"]));
   });
 }
