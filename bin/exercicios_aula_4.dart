d20703205fc9c847bfc86421d412cfaad8f247db
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-05-18 21:37:44 -0300

    Implement in Main

--- bin/exercicios_aula_4.dart
+++ bin/exercicios_aula_4.dart
@@ -1,3 +1,10 @@
+import 'package:exercicios_aula_4/db/data.dart';
+import 'package:exercicios_aula_4/patients.model.dart';
+
 void main(List<String> arguments) {
-  print('Hello word');
+  PatientModel patient = PatientModel.fromMap(map);
+
+  Map patientMap = patient.returnMap();
+
+  print(patientMap);
 }
