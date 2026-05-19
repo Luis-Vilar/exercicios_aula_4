b8c73c68302cab710c5c4a5d2f282cf006225439
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-05-18 21:36:35 -0300

    Feat : PatientModel

---
+++ lib/patients.model.dart
@@ -0,0 +1,25 @@
+import 'package:exercicios_aula_4/patients.class.dart';
+
+class PatientModel extends Patient {
+  PatientModel({
+    required super.name,
+    required super.doctor,
+    required super.tests,
+    required super.dateTime,
+  });
+
+  PatientModel.fromMap(Map map)
+    : super(
+        name: map['paciente'] as String,
+        doctor: map['medico'] as String,
+        tests: map['exames'] as List<String>,
+        dateTime: DateTime.parse(map['data']),
+      );
+
+  Map<String, dynamic> returnMap() => {
+    "name": super.name,
+    "doctor": super.doctor,
+    "tests": super.tests,
+    "dateTime": super.dateTime,
+  };
+}
